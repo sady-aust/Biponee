c89b705df8253c53b908c3d118abc2574f8d91ef
Md. Toufiqul Islam <sadydic@gmail.com>
2018-07-25 17:08:47 +0600

    initial Commit

---
+++ Biponee/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Biponee.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult CustmerAccount()
+        {
+            return View();
+        }
+
+        public ActionResult ProductPage()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+
+    }
+}
